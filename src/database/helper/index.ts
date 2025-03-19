e128ec60b8dccd13adf230abfa2b1c5a8b4d6ede
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2025-03-19 10:07:36 -0300

    test: sqlite execute query to return first result

--- src/database/helper/index.ts
+++ src/database/helper/index.ts
@@ -3,6 +3,22 @@ import {Snack} from '../../components/snack';
 import {ResultSet} from 'react-native-sqlite-storage';
 
 export class DbHelper {
+  static async find<T>(sql: string): Promise<T | undefined> {
+    try {
+      let db = await connectToDatabase();
+        let results = await db.executeSql(sql);
+
+        let response: any = {};
+        results.forEach((result: ResultSet): void => {
+            response = result.rows.item(0);
+        });
+
+        return Object.keys(response).length > 0 ? response as T : undefined;
+    } catch (error) {
+        return undefined;
+    }
+  }
+
   static async get<T>(sql: string): Promise<T[]> {
     let retorno: T[] = [];
     try {
